0fcfcdeb311452f4745d66f58b571a6efad84862
Patrick Chan <patrickchan1999@gmail.com>
2019-01-31 08:24:48 -0500

    Add a basic path validator

---
+++ internal/validators/path/validpath.go
@@ -0,0 +1,17 @@
+package validpath
+
+import "io/ioutil"
+import "os"
+
+func Valid(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	}
+
+	var d []byte
+	if err := ioutil.WriteFile(path, d, 0644); err == nil {
+		os.Remove(path)
+		return true
+	}
+	return false
+}
